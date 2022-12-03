01692d5f81c9f0bf7691533438372138d44a447a
Ilia <114323752+ltlstv@users.noreply.github.com>
2022-12-03 10:11:05 +0700

    Add files via upload

---
+++ projects/kahootbeta.py
@@ -0,0 +1,22 @@
+def quektorina():
+    import time
+    def gaycheck(score):
+        print(que)
+        for i in range(len(que)):
+            print (f'{que[i]} - ',end=' ')
+            if input()==qque[i]:
+                score+=1
+            else:
+                score-=1
+
+    print('KAHOOT VER. BETA_420')
+    time.sleep(2)
+
+    que = ['QUE1', 'QUE2']
+    qque = ['ANS1', 'ANS2']
+    score = 0
+
+    gaycheck(score)
+    print(f'SCORE: {score}')
+    time.sleep(2)
+quektorina()
