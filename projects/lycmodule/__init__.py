9fe2817e4472eeec7449df6800e226c865a78632
Ilia <114323752+ltlstv@users.noreply.github.com>
2022-12-17 09:30:53 +0700

    Delete projects/lycmodule directory

--- projects/lycmodule/__init__.py
+++
@@ -1,3 +0,0 @@
-hemming.py
-morse.py
-main.py
--- projects/lycmodule/main.py
+++
@@ -1,8 +0,0 @@
-import hemming
-import morse
-
-a=int(input(print("1 - Хемминг\n2 - Морзе\n")))
-if a == 1:
-    hemming.hemming()
-elif a == 2:
-    morse.morse()
